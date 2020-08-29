c0bc0a2f9edb6cc4f434705f9e78a2e5171a96d9
Naji El Kotob <naji@dotnetheroes.com>
2020-08-29 15:34:33 +0300

    Data Types and Variables (Console)

---
+++ Data Types and Variables (Console)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_and_Variables__Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+            //Console.WriteLine("Hello {0}", name);
+            //Console.WriteLine($"Hello {name}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
