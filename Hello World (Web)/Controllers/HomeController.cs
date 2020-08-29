0c0913cedc12b7ed1192c3ac4afc768e55fb158c
Naji El Kotob <naji@dotnetheroes.com>
2020-08-29 15:19:08 +0300

    Hello World (MVC)

---
+++ Hello World (Web)/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hello_World__Web_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hello World (Web)/readme.txt
@@ -0,0 +1,2 @@
+﻿References
+https://dzone.com/articles/setting-up-an-mvc-core-31-application-with-an-empt
