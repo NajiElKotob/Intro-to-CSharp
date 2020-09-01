49dd15b1a95c52358387f130bee29515f2526e41
Naji El Kotob <naji@dotnetheroes.com>
2020-09-01 03:15:38 +0300

    Data Types and Variables

--- Data Types and Variables (Console)/Program.cs
+++ Data Types and Variables (Console)/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,17 @@ namespace Data_Types_and_Variables__Console_
             Console.Write("Enter your name: ");
             var name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name);
+            Console.Write("Enter your next birthday date (dd/mm/yyyy): ");
+            var dob = Console.ReadLine();
+
+            // DateTime.Parse Method https://docs.microsoft.com/en-us/dotnet/api/system.datetime.parse
+            var daysUntilBirthay = ((TimeSpan)(DateTime.Parse(dob, new CultureInfo("ar-LB", false)) - DateTime.Now)).Days;
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+
+            Console.WriteLine("Hello " + name + ", " + daysUntilBirthay.ToString() + " days until your birthday");
+
+            //ToDo
             //Console.WriteLine("Hello {0}", name);
             //Console.WriteLine($"Hello {name}");
 
