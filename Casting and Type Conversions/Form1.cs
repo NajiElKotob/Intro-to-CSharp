bed8eb03a4c3378ca5dfa9dede3c0ca433ff0044
Naji El Kotob <naji@dotnetheroes.com>
2020-09-02 21:21:44 +0300

    Parse with Number Style

--- Casting and Type Conversions/Form1.cs
+++ Casting and Type Conversions/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
