079a1dfe7ccf58d23324fa083257d9ddccd74348
Naji El Kotob <naji@dotnetheroes.com>
2020-09-12 15:29:58 +0300

    Hashing

---
+++ 10. Encryption and Decryption/Hashing/Hashing/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Hashing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHash = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput1 = new System.Windows.Forms.Label();
+            this.lblOutput2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHash
+            // 
+            this.btnHash.Location = new System.Drawing.Point(22, 70);
+            this.btnHash.Name = "btnHash";
+            this.btnHash.Size = new System.Drawing.Size(75, 23);
+            this.btnHash.TabIndex = 0;
+            this.btnHash.Text = "Hash";
+            this.btnHash.UseVisualStyleBackColor = true;
+            this.btnHash.Click += new System.EventHandler(this.btnHash_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(22, 42);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(361, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblOutput1
+            // 
+            this.lblOutput1.AutoSize = true;
+            this.lblOutput1.Location = new System.Drawing.Point(22, 116);
+            this.lblOutput1.Name = "lblOutput1";
+            this.lblOutput1.Size = new System.Drawing.Size(20, 17);
+            this.lblOutput1.TabIndex = 2;
+            this.lblOutput1.Text = "...";
+            // 
+            // lblOutput2
+            // 
+            this.lblOutput2.AutoSize = true;
+            this.lblOutput2.Location = new System.Drawing.Point(22, 142);
+            this.lblOutput2.Name = "lblOutput2";
+            this.lblOutput2.Size = new System.Drawing.Size(20, 17);
+            this.lblOutput2.TabIndex = 3;
+            this.lblOutput2.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Salt";
+            // 
+            // txtSalt
+            // 
+            this.txtSalt.Location = new System.Drawing.Point(393, 42);
+            this.txtSalt.Name = "txtSalt";
+            this.txtSalt.Size = new System.Drawing.Size(100, 22);
+            this.txtSalt.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 210);
+            this.Controls.Add(this.txtSalt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOutput2);
+            this.Controls.Add(this.lblOutput1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnHash);
+            this.Name = "MainForm";
+            this.Text = "Hashing";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHash;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput1;
+        private System.Windows.Forms.Label lblOutput2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSalt;
+    }
+}
+
---
+++ 10. Encryption and Decryption/Hashing/Hashing/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Use Visual C# to compute and compare hash values
+https://docs.microsoft.com/en-us/troubleshoot/dotnet/csharp/compute-hash-values
+https://codeshare.co.uk/blog/sha-256-and-sha-512-hash-examples/
+*/
+namespace Hashing
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHash_Click(object sender, EventArgs e)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(txtSalt.Text + "_" + txtInput.Text);
+            byte[] hash = sha256.ComputeHash(bytes);
+            lblOutput1.Text = GetStringFromHash(hash);
+            lblOutput2.Text = lblOutput1.Text.Length.ToString() + " characters \n\r Note: A SHA256 is always 256 bits long, equivalent to 32 bytes, or 64 bytes in an hexadecimal string format.";
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+    }
+}
