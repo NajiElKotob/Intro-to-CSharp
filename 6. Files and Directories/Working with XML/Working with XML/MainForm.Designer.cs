4dd0713d79b2e959120f6115c25cff553b5c9b19
Naji El Kotob <naji@dotnetheroes.com>
2020-09-15 18:18:54 +0300

    Working with XML

---
+++ 6. Files and Directories/Working with XML/Working with XML/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Working_with_XML
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(12, 12);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(153, 23);
+            this.btnSerialize.TabIndex = 0;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSerialize);
+            this.Name = "MainForm";
+            this.Text = "Working with XML";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSerialize;
+    }
+}
+
---
+++ 6. Files and Directories/Working with XML/Working with XML/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Working_with_XML
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            WeatherForecastWithPOCOs weatherForecastWithPOCOs = new WeatherForecastWithPOCOs();
+
+            XmlSerializer xs = new XmlSerializer(typeof(WeatherForecastWithPOCOs));
+
+            TextWriter txtWriter = new StreamWriter(@"WeatherForecastWithPOCOs.xml");
+
+            xs.Serialize(txtWriter, weatherForecastWithPOCOs);
+
+            txtWriter.Close();
+        }
+    }
+
+
+    public class WeatherForecastWithPOCOs
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+        public string SummaryField;
+        public IList<DateTimeOffset> DatesAvailable { get; set; }
+        public Dictionary<string, HighLowTemps> TemperatureRanges { get; set; }
+        public string[] SummaryWords { get; set; }
+    }
+
+    public class HighLowTemps
+    {
+        public int High { get; set; }
+        public int Low { get; set; }
+    }
+
+
+}
