c35a8f86a8b7895d75bc31f4494bed039279da98
Naji El Kotob <naji@dotnetheroes.com>
2020-09-17 22:58:58 +0300

    File Encryption

--- 10. Encryption and Decryption/File Encryption/File Encryption/MainForm.Designer.cs
+++ 10. Encryption and Decryption/File Encryption/File Encryption/MainForm.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.btnCreateFile = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnOpenAgain = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnOpenOneMoreTime = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateFile
@@ -38,17 +43,79 @@
             this.btnCreateFile.Size = new System.Drawing.Size(161, 28);
             this.btnCreateFile.TabIndex = 0;
             this.btnCreateFile.Text = "1. Create a file";
+            this.btnCreateFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnCreateFile.UseVisualStyleBackColor = true;
             this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 47);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(161, 28);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "2. Open";
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(13, 81);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(161, 28);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "3. Encrypt";
+            this.btnEncrypt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnOpenAgain
+            // 
+            this.btnOpenAgain.Location = new System.Drawing.Point(13, 115);
+            this.btnOpenAgain.Name = "btnOpenAgain";
+            this.btnOpenAgain.Size = new System.Drawing.Size(161, 28);
+            this.btnOpenAgain.TabIndex = 3;
+            this.btnOpenAgain.Text = "4. Open";
+            this.btnOpenAgain.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpenAgain.UseVisualStyleBackColor = true;
+            this.btnOpenAgain.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(13, 149);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(161, 28);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "5. Decrypt";
+            this.btnDecrypt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnOpenOneMoreTime
+            // 
+            this.btnOpenOneMoreTime.Location = new System.Drawing.Point(13, 183);
+            this.btnOpenOneMoreTime.Name = "btnOpenOneMoreTime";
+            this.btnOpenOneMoreTime.Size = new System.Drawing.Size(161, 28);
+            this.btnOpenOneMoreTime.TabIndex = 5;
+            this.btnOpenOneMoreTime.Text = "6. Open";
+            this.btnOpenOneMoreTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpenOneMoreTime.UseVisualStyleBackColor = true;
+            this.btnOpenOneMoreTime.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 424);
+            this.ClientSize = new System.Drawing.Size(286, 363);
+            this.Controls.Add(this.btnOpenOneMoreTime);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnOpenAgain);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.btnCreateFile);
             this.Name = "MainForm";
             this.Text = "File Encryption";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +123,11 @@
         #endregion
 
         private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnOpenAgain;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnOpenOneMoreTime;
     }
 }
 
--- 10. Encryption and Decryption/File Encryption/File Encryption/MainForm.cs
+++ 10. Encryption and Decryption/File Encryption/File Encryption/MainForm.cs
@@ -8,11 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Diagnostics;
+
+/*
+File.Encrypt(String) Method
+Encrypts a file so that only the account used to encrypt the file can decrypt it.
+https://docs.microsoft.com/en-us/dotnet/api/system.io.file.encrypt
+*/
 
 namespace File_Encryption
 {
     public partial class MainForm : Form
     {
+
+        const string fileName = "Hello_CSharp.txt";
+        string applicationPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\";
+
         public MainForm()
         {
             InitializeComponent();
@@ -22,12 +34,47 @@ namespace File_Encryption
         {
             var content = "Hello C#! (Secret message)";
 
-            var sw = File.CreateText("Hello_CSharp.txt");
+            var sw = File.CreateText(fileName);
             sw.WriteLine(content);
             sw.Close();
+        }
 
+        // Encrypt a file.
+        public static void AddEncryption(string fileName)
+        {
 
+            File.Encrypt(fileName);
+        }
 
+        // Decrypt a file.
+        public static void RemoveEncryption(string fileName)
+        {
+            File.Decrypt(fileName);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile(applicationPath + fileName);
+        }
+
+        private void OpenFile(string fileName)
+        {
+            Process.Start("notepad", fileName);
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            AddEncryption(applicationPath + fileName);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            RemoveEncryption(applicationPath + fileName);
         }
     }
 }
