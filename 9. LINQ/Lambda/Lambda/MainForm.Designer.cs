8375077652ab110ad885e7238ec2664b4e33a069
Naji El Kotob <naji@dotnetheroes.com>
2020-09-26 00:19:07 +0300

    Update

--- 9. LINQ/Lambda/Lambda/MainForm.Designer.cs
+++ 9. LINQ/Lambda/Lambda/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Lambda
+﻿namespace Anonymous_Lambda_DEMO
 {
-    partial class MainForm
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Anonymous Type";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Anonymous Method";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Anonymous Method#2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(256, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(197, 51);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Lambda";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Anonymous";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Find an employee using:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(255, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(197, 51);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Delegate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(255, 168);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(197, 51);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Predicate";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(255, 114);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(197, 51);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Method";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(255, 59);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(197, 51);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "foreach";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(494, 368);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMain";
+            this.Text = "Anonymous and Lambda";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- 9. LINQ/Lambda/Lambda/MainForm.cs
+++ 9. LINQ/Lambda/Lambda/MainForm.cs
@@ -1,20 +1,204 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq;
 
-namespace Lambda
+/*
+Lambda expressions (C# reference)
+https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
+*/
+
+namespace Anonymous_Lambda_DEMO
 {
-    public partial class MainForm : Form
+    public partial class frmMain : Form
     {
-        public MainForm()
+       public frmMain()
         {
             InitializeComponent();
+        }
+
+
+        #region AnonymousType   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var num = new { a = 1, b = 2, c = 3 };
+            var num2 = new { a = 1, b = 2, c = 3 }; //1 and 2 are the same
+            var num3 = new { a = 1, b = 2, z = 3 }; //1 and 3 diff. name (z)
+            var num4 = new { a = 1, b = 2, c = 3, z = 4 }; //1 and 4 diff. count (4)
+            var num5 = new { a = 1, b = 2, c = 3.20 }; //1 and 5 diff. data type
+            var num6 = new { a = 1, c = 3, b = 2 }; //1 and 6 diff. order
+
+
+            MessageBox.Show(num.ToString());
+
+            //num.a = 100; //Error: Property or indexer 'AnonymousType#1.a' cannot be assigned to -- it is read only	
+
+
+            MessageBox.Show("Same (Name, Type, Count and Order): " + num.Equals(num2).ToString()); // same names, types, count, and order
+            MessageBox.Show("Different Names: " + num.Equals(num3).ToString()); // Different names
+            MessageBox.Show("Different Count: " + num.Equals(num4).ToString()); // Different count
+            MessageBox.Show("Different Type: " + num.Equals(num5).ToString()); // Different data type
+            MessageBox.Show("Different Order: " + num.Equals(num6).ToString()); // Different order
+
+
+            //Equals() or GetHashCode() for comparison
+            //MessageBox.Show((num.GetHashCode() == num3.GetHashCode()).ToString());
+        }
+        #endregion
+
+        #region AnonymousMethod
+
+
+
+        delegate void AnonymousMethod(int x);
+        AnonymousMethod am = delegate (int x)
+       {
+           MessageBox.Show($"Anonymous Method: {x}");
+       };
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            am(100);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Results from the anonymous delegate call.
+            pt("The delegate using the anonymous method is called.");
+
+            // The delegate instantiation using a named method
+            pt = new PrintTextDel(PrintText);
+
+            // Results from the old style delegate call.
+            pt("The delegate using the named method is called.");
+        }
+
+        // Declare a delegate. 
+        delegate void PrintTextDel(string s);
+        // Instantiate the delegate type using an anonymous method.
+        PrintTextDel pt = delegate (string t)
+        {
+            MessageBox.Show("Anony method: " + t);
+        };
+
+        // The method associated with the named delegate. 
+        void PrintText(string t)
+        {
+            MessageBox.Show("Named method: " + t);
+        }
+
+        #endregion
+
+        #region LambdaExpression
+        class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        List<Employee> employees = new List<Employee>()
+            {
+                new Employee{ ID=1, Name = "Tima"},
+                new Employee{ ID=2, Name = "Rayan"},
+                new Employee{ ID=3, Name = "Aya"},
+            };
+
+        //Using method 
+        private int empID = 0;
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Using foreach
+
+            empID = 2;
+            Employee myEmployee = null;
+            foreach (var emp in employees)
+            {
+
+                if (emp.ID == empID)
+                {
+                    myEmployee = emp;
+                    break;
+                }
+            }
+            //          if (myEmployee != null)
+            //          {
+            //MessageBox.Show(myEmployee.Name);
+            //          }else
+            //          {
+            //              MessageBox.Show("Not Found");
+            //          }
+
+            MessageBox.Show(myEmployee?.Name);
+
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Using bool method
+            empID = 2;
+            Employee myEmployee = employees.Find(SearchByID);
+            MessageBox.Show(myEmployee?.Name);
+        }
+
+        bool SearchByID(Employee emp)
+        {
+            if (emp.ID == empID)
+                return true;
+            else
+                return false;
+        }
+
+
+        //Using Predicates 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Employee myEmployee = employees.Find(ByID(2));
+            MessageBox.Show(myEmployee.Name);
+        }
+
+        static Predicate<Employee> ByID(int id)
+        {
+            return delegate (Employee employee)
+            {
+                return employee.ID == id;
+            };
+        }
+
+
+
+        //Using Delegate 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int searchForId = 2;
+            Employee myEmployee = employees.Find(delegate (Employee emp) { return emp.ID == searchForId; });
+            MessageBox.Show(myEmployee?.Name);
+        }
+
+        //Using Lambda 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int searchForId = 5;
+           // Employee myEmployee = employees.Find(emp => emp.ID == searchForId);
+            Employee myEmployee = employees.Where(emp => emp.ID == searchForId).SingleOrDefault();
+
+            MessageBox.Show(myEmployee?.Name);
+            
+        }
+
+       
+
+
+
+
+        #endregion
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
