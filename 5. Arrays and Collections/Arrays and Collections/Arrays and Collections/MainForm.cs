71af0efcea5e43761a6f7a6fea540986fa7295e8
Naji El Kotob <naji@dotnetheroes.com>
2020-09-29 10:20:17 +0300

    Arrays and Collections

---
+++ 5. Arrays and Collections/Arrays and Collections/Arrays and Collections/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Arrays_and_Collections
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/single-dimensional-arrays
+            // Declare a single-dimensional array  
+            int[] array1 = new int[6] { 10, 15, 22, 28, 31, 40 };
+            MessageBox.Show(array1.Length.ToString());
+            MessageBox.Show(array1[0].ToString());
+
+            var s = string.Empty;
+            foreach (var item in array1)
+            {
+                s += item + " ";
+            }
+
+            lblOutput.Text = s;
+
+
+            return;
+
+            // Declare and set array element values 
+            int[] array2 = new int[] { 1, 3, 5, 7, 9 };
+
+            // Alternative syntax 
+            int[] array3 = { 1, 2, 3, 4, 5, 6 };
+
+            // Declare a two dimensional array 
+            int[,] multiDimensionalArray1 = new int[2, 3];
+
+            // Declare and set array element values 
+            int[,] multiDimensionalArray3 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+
+            // A jagged array is an array whose elements are arrays. https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/jagged-arrays
+            // Declare a jagged array 
+            int[][] jaggedArray = new int[6][];
+
+            // Set the values of the first array in the jagged array structure
+            jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Queue Class https://msdn.microsoft.com/en-us/library/system.collections.queue(v=vs.110).aspx
+            // Creates and initializes a new Queue.
+
+            System.Collections.Queue queue = new System.Collections.Queue();
+            queue.Enqueue("Hello");
+            queue.Enqueue("World");
+            queue.Enqueue("!");
+
+            // Displays the properties and values of the Queue.
+            lblOutput.Text = string.Empty;
+            lblOutput.Text += string.Format("Count: {0}", queue.Count);
+            lblOutput.Text += (", Values:");
+
+            //foreach (Object obj in myQ)
+            //{
+            //    lblOutput.Text += string.Format(" {0}", obj);
+            //}
+
+            while (queue.Count > 0)
+            {
+                lblOutput.Text += string.Format(" {0}", queue.Dequeue().ToString());
+            }
+
+            
+
+       
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Collections.Generic.Queue<string> queue = new System.Collections.Generic.Queue<string>();
+            queue.Enqueue("Hello");
+            queue.Enqueue("World");
+            queue.Enqueue("!");
+
+
+            // Displays the properties and values of the Queue.
+            lblOutput.Text = string.Empty;
+            lblOutput.Text += string.Format("Count: {0}", queue.Count);
+            lblOutput.Text += (", Values:");
+
+            foreach (string s in queue)
+            {
+                lblOutput.Text += string.Format(" {0}", s);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Implements the IList interface using an array whose size is dynamically increased as required.
+            //https://docs.microsoft.com/en-us/dotnet/api/system.collections.arraylist?view=netframework-4.8
+
+            // Creates and initializes a new ArrayList.
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Hello");
+            myAL.Add("World");
+            myAL.Add("!");
+
+            // Displays the properties and values of the ArrayList.
+            lblOutput.Text = string.Format("Count: {0}", myAL.Count);
+            lblOutput.Text += "\r\n";
+            lblOutput.Text += string.Format("Capacity: {0}", myAL.Capacity);
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        //Stack Class https://docs.microsoft.com/en-us/dotnet/api/system.collections.stack?view=netcore-3.1
+
+           
+            // Creates and initializes a new Stack.
+            Stack myStack = new Stack();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+
+            // Displays the properties and values of the Queue.
+            lblOutput.Text = string.Empty;
+            lblOutput.Text += string.Format("Count: {0}", myStack.Count);
+            lblOutput.Text += (", Values:");
+
+            foreach (string s in myStack)
+            {
+                lblOutput.Text += string.Format(" {0}", s);
+            }
+
+        }
+
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 5. Arrays and Collections/Arrays and Collections/Arrays and Collections/MainForm.designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Arrays_and_Collections
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnArrayList = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Arrays";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Queue (FIFO)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblOutput.Location = new System.Drawing.Point(0, 194);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(569, 75);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "...";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 38);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Queue<T>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnArrayList
+            // 
+            this.btnArrayList.Location = new System.Drawing.Point(11, 92);
+            this.btnArrayList.Name = "btnArrayList";
+            this.btnArrayList.Size = new System.Drawing.Size(149, 38);
+            this.btnArrayList.TabIndex = 6;
+            this.btnArrayList.Text = "ArraysList";
+            this.btnArrayList.UseVisualStyleBackColor = true;
+            this.btnArrayList.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(352, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(149, 38);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Stack";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Priority-based";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Index-based";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Priority-based";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 269);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnArrayList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Arrays and Collections";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnArrayList;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
