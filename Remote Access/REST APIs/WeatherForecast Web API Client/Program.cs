1b399e6ea713ac342decb04b324e29d93418d519
Naji El Kotob <naji@dotnetheroes.com>
2020-09-29 14:43:06 +0300

    WeatherForecast Web API Client

---
+++ Remote Access/REST APIs/WeatherForecast Web API Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast_Web_API_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+    }
+}
