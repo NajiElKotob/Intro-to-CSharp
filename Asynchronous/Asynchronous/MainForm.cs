7ac8979cf1c73dcb419862c3b3815557319ba867
Naji El Kotob <naji@dotnetheroes.com>
2020-10-08 17:45:39 +0300

    Asynchronous

---
+++ Asynchronous/Asynchronous/MainForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitDEMO
+{
+    public partial class MainForm : Form
+    {
+
+        int startCharCode = 88;
+        int endCharCode = 90;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private List<string> PrepareFilesList()
+        {
+            List<string> listOfFiles = new List<string>();
+
+            listOfFiles.Add("c:\\file1.pdf");
+            listOfFiles.Add("c:\\file2.pdf");
+            listOfFiles.Add("c:\\file3.pdf");
+
+            return listOfFiles;
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Sync -----------------");
+
+            Stopwatch stopWatch = new Stopwatch(); //https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?redirectedfrom=MSDN&view=netframework-4.7.2
+            stopWatch.Start();
+
+            List<string> files = PrepareFilesList();
+            foreach (var file in files)
+            {
+                listBox1.Items.Add(ScanFile(file));
+            }
+
+
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed; // Get the elapsed time as a TimeSpan value.
+            listBox1.Items.Add(ts.ToString());
+            listBox1.Items.Add("=======================");
+        }
+
+        private async void btnAsync_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Async -----------------");
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            List<string> files = PrepareFilesList();
+            foreach (var file in files)
+            {
+                var r = await Task.Run(() => ScanFile(file));
+                listBox1.Items.Add(r);
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            listBox1.Items.Add(ts.ToString());
+            listBox1.Items.Add("=======================");
+        }
+
+        private async void btnParallel_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Parallel -----------------");
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            await RunTasksParallelAsync();
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed; // Get the elapsed time as a TimeSpan value.
+            listBox1.Items.Add(ts.ToString());
+            listBox1.Items.Add("=======================");
+        }
+
+
+        private async Task RunTasksParallelAsync()
+        {
+            List<Task<string>> tasks = new List<Task<string>>();
+
+
+            List<string> files = PrepareFilesList();
+            foreach (var file in files)
+            {
+                tasks.Add(Task.Run(() => ScanFile(file)));
+            }
+
+
+            var results = await Task.WhenAll(tasks);
+
+
+            foreach (var r in results)
+            {
+                listBox1.Items.Add(r);
+            }
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+
+        private string ScanFile(string fileName)
+        {
+            System.Threading.Thread.Sleep(2000);
+            return fileName + " scanned on " + DateTime.Now.ToString();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    
+    }
+
+
+}
---
+++ Asynchronous/Asynchronous/MainForm.designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AsyncAwaitDEMO
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.btnParallel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(171, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(359, 340);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(12, 12);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(142, 46);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(12, 64);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(142, 46);
+            this.btnAsync.TabIndex = 3;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // btnParallel
+            // 
+            this.btnParallel.Location = new System.Drawing.Point(12, 116);
+            this.btnParallel.Name = "btnParallel";
+            this.btnParallel.Size = new System.Drawing.Size(142, 46);
+            this.btnParallel.TabIndex = 4;
+            this.btnParallel.Text = "Parallel";
+            this.btnParallel.UseVisualStyleBackColor = true;
+            this.btnParallel.Click += new System.EventHandler(this.btnParallel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(455, 379);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 414);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnParallel);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MainForm";
+            this.Text = "Asynchronous";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.Button btnParallel;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
