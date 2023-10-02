ba61c992c9eb38fae6612e4feabe5c7188b28f3d
najielkotob <naji@dotnetheroes.com>
2023-10-02 22:45:57 +0400

    Implement QR code

--- Library Demos/QRGenDemo/MainForm.cs
+++ Library Demos/QRGenDemo/MainForm.cs
@@ -1,6 +1,7 @@
 using System.Windows.Forms;
 using ZXing;
 using ZXing.QrCode;
+using ZXing.Windows.Compatibility;
 
 namespace QRGenDemo;
 
@@ -13,6 +14,30 @@ public partial class MainForm : Form
 
     private void GenerateQRButton_Click(object sender, EventArgs e)
     {
-    
+        // Get the text you want to encode in the QR code
+        string textToEncode = QRCodeTextBox.Text;
+
+        // Set the barcode format to QR code
+        QrCodeEncodingOptions options = new()
+        {
+            DisableECI = true,
+            CharacterSet = "UTF-8",
+            Width = 300,
+            Height = 300
+        };
+        // Create a BarcodeWriter instance
+        BarcodeWriter barcodeWriter = new()
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = options
+        };
+
+        //barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+        // Generate the QR code as a Bitmap
+        Bitmap qrCodeBitmap = barcodeWriter.Write(textToEncode);
+
+        // Display the QR code in the PictureBox
+        QRCodePictureBox.Image = qrCodeBitmap;
     }
 }
--- Library Demos/QRGenDemo/readme.txt
+++ Library Demos/QRGenDemo/readme.txt
@@ -1,3 +1,6 @@
 ﻿NuGet packages:
 ZXing.Net
 ZXing.Net Bindings for Windows Compatibility
+
+Create Your Own QR Codes with ZXing.NET
+https://www.codeproject.com/Articles/5357944/Create-Your-Own-QR-Codes-with-ZXing-NET
