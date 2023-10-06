1702b3f93e7fc3ba0e5fd47957e801e1ccefeab1
najielkotob <naji@dotnetheroes.com>
2023-10-06 20:11:24 +0400

    Update Async Basic Console App

--- Asynchronous/AsyncBasic.ConsoleApp/Program.cs
+++ Asynchronous/AsyncBasic.ConsoleApp/Program.cs
@@ -1,26 +1,37 @@
-﻿Console.WriteLine("Starting...");
+﻿Console.WriteLine("Sync Starting...");
 
-Task<string> task1 = GetStringAfterDelay("Task 1", 3000);
-Task<string> task2 = GetStringAfterDelay("Task 2", 1000);
+string task1 = GetStringAfterDelay("Task 1", 3000);
+string task2 = GetStringAfterDelay("Task 2", 1000);
+
+Console.WriteLine(task1);
+Console.WriteLine(task2);
+
+
+static string GetStringAfterDelay(string taskName, int delay)
+{
+    Console.WriteLine($"{taskName} started.");
+
+    Task.Delay(delay); // This will cause a 2-second pause before executing the next line.
+
+    Console.WriteLine($"{taskName} completed.");
+    return $"{taskName} finished after {delay}ms";
+}
+
+
+// ============================================================
+
+
+Console.WriteLine("Async Starting...");
+
+Task<string> taskAsync1 = GetStringAfterDelayAsync("Task 1", 3000);
+Task<string> taskAsync2 = GetStringAfterDelayAsync("Task 2", 1000);
 
 // Wait for both tasks to complete simultaneously
-/**/
-string[] results = await Task.WhenAll(task1, task2);
-Console.WriteLine(results[0]);
-Console.WriteLine(results[1]);
+string[] resultsAsync = await Task.WhenAll(taskAsync1, taskAsync2);
+Console.WriteLine(resultsAsync[0]);
+Console.WriteLine(resultsAsync[1]);
 
-
-// Wait for both tasks to complete using await
-/*
-string result1 = await task1;
-string result2 = await task2;
-
-Console.WriteLine(result1);
-Console.WriteLine(result2);
-*/
-
-
-static async Task<string> GetStringAfterDelay(string taskName, int delay)
+static async Task<string> GetStringAfterDelayAsync(string taskName, int delay)
 {
     Console.WriteLine($"{taskName} started.");
 
