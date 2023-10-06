720768143c2a3c64cd32864142f957a6fdbde978
najielkotob <naji@dotnetheroes.com>
2023-10-06 09:26:16 +0400

    Async Basic Console App

---
+++ Asynchronous/AsyncBasic.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Starting...");
+
+Task<string> task1 = GetStringAfterDelay("Task 1", 3000);
+Task<string> task2 = GetStringAfterDelay("Task 2", 1000);
+
+// Wait for both tasks to complete simultaneously
+/**/
+string[] results = await Task.WhenAll(task1, task2);
+Console.WriteLine(results[0]);
+Console.WriteLine(results[1]);
+
+
+// Wait for both tasks to complete using await
+/*
+string result1 = await task1;
+string result2 = await task2;
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+*/
+
+
+static async Task<string> GetStringAfterDelay(string taskName, int delay)
+{
+    Console.WriteLine($"{taskName} started.");
+    await Task.Delay(delay);
+    Console.WriteLine($"{taskName} completed.");
+    return $"{taskName} finished after {delay}ms";
+}
+
